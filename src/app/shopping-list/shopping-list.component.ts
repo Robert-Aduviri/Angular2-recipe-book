374b7f9b99d6a975c49bc8061297a1272b306ceb
Robert Alonso <robert.alonso.ac@gmail.com>
2017-03-25 22:14:33 -0500

    Sectioning and shopping-list component details

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model'
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Chicken', 5),
+    new Ingredient('Meat', 4),
+    new Ingredient('Fish',6),
+    new Ingredient('Rice', 8)
+  ];
 
   constructor() { }
 
